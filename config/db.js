9320b4849663ced623c5ae73859bbdeea5d68b12
Saúl Díaz <saul.diaz.v@gmail.com>
2021-07-10 09:00:53 -0500

    Feature: Mongoose warn
    Mongoose 5.7.1 was release and seems to fix the issue, so setting up the useUnifiedTopology option work as expected.

--- config/db.js
+++ config/db.js
@@ -2,4 +2,4 @@ require('dotenv').config();
 
 exports.url = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?authSource=admin';
 
-exports.options =  { useNewUrlParser: true };
+exports.options =  { useNewUrlParser: true, useUnifiedTopology: true };
